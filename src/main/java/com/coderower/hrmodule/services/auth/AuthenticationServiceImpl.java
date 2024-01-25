dcdc176032d0f2be1873e25f43a3836d1f1af0eb
sameekshasxn12 <sameeksha.saxena@coderower.com>
2024-01-25 20:41:09 +0530

    signin_functionality

--- src/main/java/com/coderower/hrmodule/services/auth/AuthenticationServiceImpl.java
+++ src/main/java/com/coderower/hrmodule/services/auth/AuthenticationServiceImpl.java
@@ -1,14 +1,41 @@
 package com.coderower.hrmodule.services.auth;
 
 
+import com.coderower.hrmodule.database.entities.User;
+import com.coderower.hrmodule.database.repositories.UserRepository;
+import com.coderower.hrmodule.models.auth.AuthUser;
 import com.coderower.hrmodule.models.auth.AuthenticationRequest;
+import com.coderower.hrmodule.services.JwtService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+	
+	@Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
     public String signIn(AuthenticationRequest authenticationRequest) {
-        return  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDkzNjcwZjQ2MjA5ZDRlZTFmNjYzMSIsImlhdCI6MTcwNTcyNTA3NiwiZXhwIjoxNzA2MzI5ODc2fQ.9P_nUsD-avCa__3KTIusREp25wQ1xntXeMkYzgdDkSI";
+        User user = userRepository.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (!user.getPassword().equals(authenticationRequest.getPassword())) {
+            throw new UsernameNotFoundException("Invalid password");
+        }
+
+        UserDetails userDetails = new AuthUser(user);
+        return jwtService.generateToken(userDetails);
+    
+
     }
+    
+    
 }
