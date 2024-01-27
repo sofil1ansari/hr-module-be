af6dbfd54de7cbdac05909cb3f33a8cfac876e29
sameekshasxn12 <sameeksha.saxena@coderower.com>
2024-01-27 14:45:19 +0530

    changes

--- src/main/java/com/coderower/hrmodule/database/repositories/ProjectRepository.java
+++ src/main/java/com/coderower/hrmodule/database/repositories/ProjectRepository.java
@@ -1,9 +1,7 @@
 package com.coderower.hrmodule.database.repositories;
 
 import com.coderower.hrmodule.database.entities.Project;
-
 import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
